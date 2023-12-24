419b8b0b0bb42361a751d397cec170cb6854a221
hakan <tasbeyhakan@gmail.com>
2023-12-24 23:57:50 +0300

    first commit

---
+++ src/components/Question.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { arrowdown, arrowup } from "../helper/icons";
+const Card = ({item}) => {
+  const [show, setShow] = React.useState(false);
+  return (
+    <div className="card-group">
+         
+        </div>
+  );
+};
+
+export default Card;
