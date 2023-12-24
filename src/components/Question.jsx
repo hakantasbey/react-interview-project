081e56397e9119a49a62833704108d8c5ec35b0d
hakan <tasbeyhakan@gmail.com>
2023-12-24 23:59:11 +0300

    new commit

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,7 +4,15 @@ const Card = ({item}) => {
   const [show, setShow] = React.useState(false);
   return (
     <div className="card-group">
-         
+          <div className="card">
+            <div className="ques-answer">
+              <h5>{item.question}</h5>
+              <button className="btn-minus" onClick={() => setShow(!show)}>
+                {show ? arrowup : arrowdown}
+              </button>
+            </div>
+            <p>{show && item.answer}</p>
+          </div>
         </div>
   );
 };
